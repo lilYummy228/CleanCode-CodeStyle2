607b68da15627c2e2d4db11de536b370e3008be2
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-29 11:24:19 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle2/Program.cs
@@ -0,0 +1,12 @@
+﻿class Weapon
+{
+    private const int Discharge = 0;
+
+    private int _shotBulletCount = 1;
+    private int _bulletCount;
+
+    public bool IsOutOfAmmo() => _bulletCount == Discharge;
+
+    public void Shoot() => _bulletCount -= _shotBulletCount;
+}
+
