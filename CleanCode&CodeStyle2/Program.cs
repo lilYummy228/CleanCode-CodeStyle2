5c8068d7d357cc8fc5c3d89fae7d4e577848cf4f
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-30 13:08:50 +0500

    fix

--- CleanCode&CodeStyle2/Program.cs
+++ CleanCode&CodeStyle2/Program.cs
@@ -1,16 +1,22 @@
-﻿class Weapon
+﻿using System;
+
+public class Weapon
 {
     private const int Discharge = 0;
-    private const int ShotBulletsCount = 1;
 
     private int _bulletCount;
 
-    public bool IsOutOfAmmo() => _bulletCount == Discharge;
+    public Weapon(int bulletCount) =>
+        _bulletCount = bulletCount;
+
+    public bool CanShoot => 
+        _bulletCount > Discharge;
 
     public void Shoot()
     {
-        if (IsOutOfAmmo() == false)
-            _bulletCount -= ShotBulletsCount;
+        if (CanShoot == false)
+            throw new InvalidOperationException($"{nameof(_bulletCount)} is less than or equal to zero");   
+
+        _bulletCount--;
     }
 }
-
