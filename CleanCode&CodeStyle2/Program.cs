3399cfd78029e1b9bd12077e6ca507e5b874097e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-30 14:43:50 +0500

    fix

--- CleanCode&CodeStyle2/Program.cs
+++ CleanCode&CodeStyle2/Program.cs
@@ -2,21 +2,23 @@
 
 public class Weapon
 {
-    private const int Discharge = 0;
-
     private int _bulletCount;
+    private int _shotCount;
 
-    public Weapon(int bulletCount) =>
-        _bulletCount = bulletCount;
+    public Weapon(int bulletCount, int shotCount)
+    {
+        _bulletCount = bulletCount >= 0 ? bulletCount : throw new ArgumentOutOfRangeException($"{nameof(bulletCount)} cannot be less than zero");
+        _shotCount = shotCount > 0 ? shotCount : throw new ArgumentOutOfRangeException($"{nameof(bulletCount)} cannot be less than or equal to zero");
+    }
 
-    public bool CanShoot => 
-        _bulletCount > Discharge;
+    public bool CanShoot => _bulletCount > 0 && _bulletCount >= _shotCount;
 
     public void Shoot()
     {
         if (CanShoot == false)
-            throw new InvalidOperationException($"{nameof(_bulletCount)} is less than or equal to zero");   
+            throw new InvalidOperationException($"{nameof(_bulletCount)} is less than or equal to zero");
 
-        _bulletCount--;
+        _bulletCount -= _shotCount;
+
     }
 }
